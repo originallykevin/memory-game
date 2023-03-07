2e7f6e4879a22c275553abfbf01625b3b4fdd580
Kevin Ly <ly1@ualberta.ca>
2023-03-06 22:31:11 -0700

    create initial grid with blank being shown

---
+++ app.js
@@ -0,0 +1,72 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // card options
+  const cardArrays = [
+    {
+      name: 'cheeseburger',
+      img: 'images/cheeseburger.png'
+    },
+    {
+      name: 'cheeseburger',
+      img: 'images/cheeseburger.png'
+    },
+    {
+      name: 'fries',
+      img: 'images/fries.png'
+    },
+    {
+      name: 'fries',
+      img: 'images/fries.png'
+    },
+    {
+      name: 'hotdog',
+      img: 'images/hotdog.png'
+    },
+    {
+      name: 'hotdog',
+      img: 'images/hotdog.png'
+    },
+    {
+      name: 'ice-cream',
+      img: 'images/ice-cream.png'
+    },
+    {
+      name: 'ice-cream',
+      img: 'images/ice-cream.png'
+    },
+    {
+      name: 'milkshake',
+      img: 'images/milkshake.png'
+    },
+    {
+      name: 'milkshake',
+      img: 'images/milkshake.png'
+    },
+    {
+      name: 'pizza',
+      img: 'images/pizza.png'
+    },
+    {
+      name: 'pizza',
+      img: 'images/pizza.png'
+    },
+  ];
+
+  const grid = document.querySelector('.grid');
+  // game board
+  const createBoard = function() {
+    for (let i = 0; i < cardArrays.length; i++) {
+      const card = document.createElement('img');
+      card.setAttribute('src', 'images/blank.png');
+      card.setAttribute('data-id', i);
+      // card.addEventListener('click', flipcard)
+      grid.appendChild(card);
+    }
+  };
+
+  createBoard()
+
+
+
+
+});
