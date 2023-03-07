14479cc5187dfe78afbf1b5dc241c94a79ccf5dc
Kevin Ly <ly1@ualberta.ca>
2023-03-06 23:55:50 -0700

    add flip card and check matches function

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   // card options
-  const cardArrays = [
+  const cardArray = [
     {
       name: 'cheeseburger',
       img: 'images/cheeseburger.png'
@@ -52,21 +52,63 @@ document.addEventListener('DOMContentLoaded', () => {
     },
   ];
 
+  // randomize card array on refresh or new game
+  cardArray.sort(() => 0.5 - Math.random());
+
   const grid = document.querySelector('.grid');
+  const resultDisplay = document.querySelector('#result');
+  let cardsChosen = [];
+  let cardsChosenId = [];
+  let cardsWon = [];
+
   // game board
   const createBoard = function() {
-    for (let i = 0; i < cardArrays.length; i++) {
+    for (let i = 0; i < cardArray.length; i++) {
       const card = document.createElement('img');
       card.setAttribute('src', 'images/blank.png');
       card.setAttribute('data-id', i);
-      // card.addEventListener('click', flipcard)
+      card.addEventListener('click', flipCard);
       grid.appendChild(card);
     }
   };
 
-  createBoard()
 
+  // check for matches
+  const checkForMatch = function() {
+    const cards = document.querySelectorAll('img');
+    const optionsOneId = cardsChosenId[0];
+    const optionsTwoId = cardsChosenId[1];
+    if (cardsChosen[0] === cardsChosen[1]) {
+      alert('Match found');
+      cards[optionsOneId].setAttribute('src', 'images/white.png');
+      cards[optionsTwoId].setAttribute('src', 'images/white.png');
+      cardsWon.push(cardsChosen);
+    } else {
+      cards[optionsOneId.setAttribute('src', 'images/blank.png')];
+      cards[optionsTwoId.setAttribute('src', 'images/blank.png')];
+      alert('Please try again');
+    }
 
+    cardsChosen = [];
+    cardsChosenId = [];
+    resultDisplay.textContent = cardsWon.length;
+    if (cardsWon.length === cardArray.length / 2) {
+      resultDisplay.textContent = "You've won!";
+    }
+  };
+
+  // flipping card
+  const flipCard = function() {
+    const cardId = this.getAttribute('data-id');
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute('src', cardArray[cardId].img);
+    if (cardsChosen.length === 2) {
+      setTimeout(checkForMatch, 500);
+    }
+  };
+
+  createBoard();
 
 
 });
